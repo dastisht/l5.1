b175c7aede28e76b6d564fd7553ee89147acfce1
Yurii Tsal-Tsalko <dastisht@gmail.com>
2023-10-09 17:50:39 +0200

    commit 1

--- models.py
+++ models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.utils import timezone
 
+class Product(models.Model):
+    
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    added_date = models.DateTimeField(default=timezone.now)
+    
+    image = models.ImageField(upload_to='images', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Client(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+from pathlib import Path
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
